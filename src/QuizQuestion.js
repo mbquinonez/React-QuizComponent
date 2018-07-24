c0265a97e93b20fb89c1b7af9bb102c90bc8639f
Michael Quinonez <michael.quinonez@certent.com>
2018-07-24 11:32:25 -0700

    Added Incorrect Answer Message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,28 +2,46 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            incorrectAnswer : false,
+        };
+    }
+
     render() {
         return (
-        <main>
-            <section>
-                <p>{this.props.quiz_question.instruction_text}</p>
-            </section>
-            <section className="buttons">
-                <ul>
-                    {this.props.quiz_question.answer_options.map((answer_option, index) => 
-                        <QuizQuestionButton key={index}
-                            button_text={answer_option}
-                            clickHandler={this.handleClick.bind(this)}/> 
-                    )}
-                </ul>
-            </section>
-        </main>
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => 
+                            <QuizQuestionButton key={index}
+                                button_text={answer_option}
+                                clickHandler={this.handleClick.bind(this)}/> 
+                        )}
+                    </ul>
+                </section>
+                {this.state.incorrectAnswer ? <div className='error'>Sorry, that's not right</div> : null}
+            </main>
         );
     }
 
     handleClick(buttonText) {
-        if(buttonText === this.props.quiz_question.answer)
+        if(buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
+            this.setState({
+                incorrectAnswer: false,
+            });
+        }
+        else {
+            this.setState({
+                incorrectAnswer: true,
+            });
+        }
     }
 }
 
